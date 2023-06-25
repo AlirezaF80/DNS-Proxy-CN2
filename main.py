26ce5df2690ed7ec14f6aaafece4d25247bdf9a5
AlirezaF80 <alirezafarzaneh@email.kntu.ac.ir>
2023-06-25 19:59:08 +0330

    Implement a simple forwarding DNS Proxy Server

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import socket
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class DNSProxyServer:
+    def __init__(self, DNSserverIP, host_address, port):
+        self.DNSserverIP = DNSserverIP
+        self.host_address = host_address
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def start(self):
+        try:
+            self.socket.bind((self.host_address, self.port))
+            print("UDP DNS proxy server started. Listening on port", self.port)
+
+            while True:
+                data, addr = self.socket.recvfrom(1024)
+                self._handle_request(data, addr)
+                print("Request from", addr, "processed")
+                print(data.hex())
+                print("\n")
+        except Exception as e:
+            print("Error starting DNS proxy server:", e)
+        finally:
+            self.socket.close()
+
+    def _handle_request(self, data, addr):
+        try:
+            response = self._send_query(data)
+            self.socket.sendto(response, addr)
+        except Exception as e:
+            print("Error processing DNS request:", e)
+
+    def _send_query(self, query):
+        server = (self.DNSserverIP, 53)
+        query_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        query_socket.sendto(query, server)
+        response, _ = query_socket.recvfrom(1024)
+        query_socket.close()
+        return response
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    DNSserverIP = '185.51.200.2'
+    host_address = '127.0.0.1'
+    port = 53
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    dns_proxy = DNSProxyServer(DNSserverIP, host_address, port)
+    dns_proxy.start()
