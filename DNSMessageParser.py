ba47669dc443d9cbce0e74b08efd18fe99067ca5
AlirezaF80 <alirezafarzaneh@email.kntu.ac.ir>
2023-06-25 23:23:08 +0330

    Separated DNSMessage Parsing and Instance creation

--- DNSMessageParser.py
+++ DNSMessageParser.py
@@ -35,13 +35,28 @@ class DNSMessageQuery:
 
 
 class DNSMessage:
-    def __init__(self, message):
-        self.message = message
-        self.header = self.parse_header()
-        self.queries = self.parse_questions()
+    def __init__(self, header, queries, answers, authority, additional):
+        self.header = header
+        self.queries = queries
+        self.answers = answers
+        self.authority = authority
+        self.additional = additional
 
-    def parse_header(self):
-        header_data = self.message[:12]
+
+class DNSMessageParser:
+    @staticmethod
+    def parse(message):
+        message = message
+        header = DNSMessageParser.parse_header(message)
+        queries = DNSMessageParser.parse_questions(message, header.questions_num)
+        answers = []
+        authority = []
+        additional = []
+        return DNSMessage(header, queries, answers, authority, additional)
+
+    @staticmethod
+    def parse_header(message) -> DNSMessageHeader:
+        header_data = message[:12]
         transaction_id = (header_data[0] << 8) + header_data[1]
         flags = (header_data[2] << 8) + header_data[3]
         questions_num = (header_data[4] << 8) + header_data[5]
@@ -51,28 +66,29 @@ class DNSMessage:
 
         return DNSMessageHeader(transaction_id, flags, questions_num, answers_num, auth_num, additional_num)
 
-    def parse_questions(self):
-        if self.header.questions_num == 0:
+    @staticmethod
+    def parse_questions(message, question_num) -> list[DNSMessageQuery]:
+        if question_num == 0:
             return []
-        queries = []
-        cur_offset = 12
 
-        for _ in range(self.header.questions_num):
+        cur_offset = 12
+        queries = []
+        for _ in range(question_num):
             qname_parts = []
             while True:
-                length = self.message[cur_offset]
+                length = message[cur_offset]
                 if length == 0:
                     break
 
                 cur_offset += 1
-                query_part = self.message[cur_offset:cur_offset + length].decode('utf-8')
+                query_part = message[cur_offset:cur_offset + length].decode('utf-8')
                 qname_parts.append(query_part)
                 cur_offset += length
             query_name = '.'.join(qname_parts)
             cur_offset += 1
-            query_type = (self.message[cur_offset] << 8) + self.message[cur_offset + 1]
+            query_type = (message[cur_offset] << 8) + message[cur_offset + 1]
             cur_offset += 2
-            query_class = (self.message[cur_offset] << 8) + self.message[cur_offset + 1]
+            query_class = (message[cur_offset] << 8) + message[cur_offset + 1]
             dns_query = DNSMessageQuery(query_name, query_type, query_class)
             queries.append(dns_query)
             cur_offset += 2
@@ -83,7 +99,7 @@ class DNSMessage:
 if __name__ == '__main__':
     dns_message_hex = '00028180000100010000000006676f6f676c6503636f6d0000010001c00c00010001000000b50004d8ef2678'
     dns_message_hex = bytes.fromhex(dns_message_hex)
-    dns_message = DNSMessage(dns_message_hex)
+    dns_message = DNSMessageParser.parse(dns_message_hex)
 
     print('Header:', dns_message.header)
     print('Queries:', dns_message.queries)
