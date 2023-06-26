6bc4a2a0dafc2ffbe89f5023469576a4a2e62219
Mohammad <s.mohammad.salari79@gmail.com>
2023-06-26 10:28:22 +0330

    redis done
    implement cache by redis.

--- cache.py
+++ cache.py
@@ -1,27 +1,41 @@
 import time
+import redis
 
 
 class Cache:
     def __init__(self, ttl):
-        self.cache = {}
+        self.redis = redis.Redis(host='localhost', port=6379, db=0)
         self.ttl = ttl
 
     def add_record(self, hostname, ip_address):
-        expiration_time = time.time() + self.ttl
-        self.cache[hostname] = (ip_address, expiration_time)
+        expiration_time = int(time.time()) + self.ttl
+        self.redis.set(hostname, ip_address, ex=self.ttl)
 
     def get_record(self, hostname):
-        if hostname in self.cache:
-            ip_address, expiration_time = self.cache[hostname]
-            if expiration_time > time.time():
-                return ip_address
-            else:
-                del self.cache[hostname]
+        ip_address = self.redis.get(hostname)
+        if ip_address:
+            return ip_address.decode()
         return None
 
     def remove_record(self, hostname):
-        if hostname in self.cache:
-            del self.cache[hostname]
+        self.redis.delete(hostname)
 
     def clear_cache(self):
-        self.cache = {}
+        self.redis.flushdb()
+
+
+# Example usage
+cache = Cache(ttl=3600)  # TTL of 1 hour
+
+# Add a record to the cache
+cache.add_record('example.com', '192.168.1.100')
+
+# Get a record from the cache
+ip = cache.get_record('example.com')
+print(ip)  # Output: 192.168.1.100
+
+# Remove a record from the cache
+cache.remove_record('example.com')
+
+# Clear the entire cache
+cache.clear_cache()
