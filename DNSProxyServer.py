f1a0c451a698cc6d04fabd155c0611f44a2a0d0c
AlirezaF80 <alirezafarzaneh@email.kntu.ac.ir>
2023-06-26 17:36:52 +0330

    Integrated DNS Message Parsing/Serialization into Server

--- DNSProxyServer.py
+++ DNSProxyServer.py
@@ -1,40 +1,41 @@
 import socket
 
+from DNSMessageParser import *
+
 
 class DNSProxyServer:
     def __init__(self, DNSserverIP, host_address, port):
         self.DNSserverIP = DNSserverIP
         self.host_address = host_address
         self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.request_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def start(self):
         try:
-            self.socket.bind((self.host_address, self.port))
+            self.request_socket.bind((self.host_address, self.port))
             print("UDP DNS proxy server started. Listening on port", self.port)
 
             while True:
-                data, addr = self.socket.recvfrom(1024)
-                self._handle_request(data, addr)
-                print("Request from", addr, "processed")
-                print(data.hex())
-                print("\n")
+                data, requester_addr = self.request_socket.recvfrom(1024)
+                dns_query = DNSMessageParser.parse(data)
+                self._handle_request(dns_query, requester_addr)
         except Exception as e:
             print("Error starting DNS proxy server:", e)
         finally:
-            self.socket.close()
+            self.request_socket.close()
 
-    def _handle_request(self, data, addr):
+    def _handle_request(self, dns_query, requester_address):
         try:
-            response = self._send_query(data)
-            self.socket.sendto(response, addr)
+            response = self._send_query(dns_query)
+            print("Response from", requester_address, "processed")
+            self.request_socket.sendto(bytes(response), requester_address)
         except Exception as e:
             print("Error processing DNS request:", e)
 
-    def _send_query(self, query):
+    def _send_query(self, dns_query):
         server = (self.DNSserverIP, 53)
         query_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        query_socket.sendto(query, server)
-        response, _ = query_socket.recvfrom(1024)
+        query_socket.sendto(bytes(dns_query), server)
+        dns_answer, _ = query_socket.recvfrom(1024)
         query_socket.close()
-        return response
+        return DNSMessageParser.parse(dns_answer)
