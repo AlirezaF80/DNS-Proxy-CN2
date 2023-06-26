f64811299d70dce98fade5a7a16bfad8011b894e
AlirezaF80 <alirezafarzaneh@email.kntu.ac.ir>
2023-06-26 18:03:14 +0330

    Renamed variables in Main

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 from DNSProxyServer import DNSProxyServer
 
 if __name__ == '__main__':
-    DNSserverIP = '185.51.200.2'  # Shecan DNS server
-    host_address = '127.0.0.1'
-    port = 53
+    dns_server_ip = '185.51.200.2'  # Shecan DNS server
+    server_host_address = '127.0.0.1'
+    server_port = 53
 
-    dns_proxy = DNSProxyServer(DNSserverIP, host_address, port)
+    dns_proxy = DNSProxyServer(dns_server_ip, server_host_address, server_port)
     dns_proxy.start()
