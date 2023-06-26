c0c044aeaecd667b882eb747494b6c7571c595f7
Mohammad <s.mohammad.salari79@gmail.com>
2023-06-26 11:39:54 +0330

    new cache with test

--- cache.py
+++ cache.py
@@ -1,14 +1,12 @@
 import time
 import redis
 
-
 class Cache:
     def __init__(self, ttl):
         self.redis = redis.Redis(host='localhost', port=6379, db=0)
         self.ttl = ttl
 
     def add_record(self, hostname, ip_address):
-        expiration_time = int(time.time()) + self.ttl
         self.redis.set(hostname, ip_address, ex=self.ttl)
 
     def get_record(self, hostname):
@@ -26,17 +24,27 @@ class Cache:
     def is_record_cached(self, hostname):
         return self.redis.exists(hostname)
 
-
 # Example usage
-cache = Cache(ttl=3600)  # TTL of 1 hour
+cache = Cache(ttl=5)  # TTL of 5 seconds
 
 # Add a record to the cache
 cache.add_record('example.com', '192.168.1.100')
 
-# Check if a record is already cached
+# Check if the record is cached immediately after adding
 is_cached = cache.is_record_cached('example.com')
 print(is_cached)  # Output: True
 
-# Check if a non-existing record is cached
-is_cached = cache.is_record_cached('example.org')
+# Retrieve the record immediately after adding
+ip = cache.get_record('example.com')
+print(ip)  # Output: 192.168.1.100
+
+# Wait for the TTL to expire
+time.sleep(6)
+
+# Check if the record is still cached after the TTL has expired
+is_cached = cache.is_record_cached('example.com')
 print(is_cached)  # Output: False
+
+# Retrieve the record after the TTL has expired
+ip = cache.get_record('example.com')
+print(ip)  # Output: None
