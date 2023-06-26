b9169bd2028416c428092a9f459ba35e3ee0459c
AlirezaF80 <alirezafarzaneh@email.kntu.ac.ir>
2023-06-26 18:01:44 +0330

    Removed settings import from Cache
    Now cache takes ttl, redist_host and redis_port as arguments to its constructor

--- Cache.py
+++ Cache.py
@@ -5,30 +5,9 @@ import logging
 
 
 class Cache:
-    def __init__(self, settings_file):
-        self.redis = redis.Redis(host='localhost', port=6379, db=0)
-        self.load_settings(settings_file)
-
-    def setup_logger(self):
-        logger = logging.getLogger('Cache')
-        logger.setLevel(logging.DEBUG)
-
-        log_filename = 'cache.log'
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-        file_handler = logging.FileHandler(log_filename)
-        file_handler.setLevel(logging.DEBUG)
-        file_handler.setFormatter(formatter)
-
-        logger.addHandler(file_handler)
-
-        return logger
-
-    def load_settings(self, settings_file):
-        with open(settings_file, 'r') as file:
-            settings = json.load(file)
-        self.ttl = settings.get('cache-expiration-time', 3600)
-        self.external_dns_servers = settings.get('external-dns-servers', [])
+    def __init__(self, cache_ttl, redis_host, redis_port):
+        self.redis = redis.Redis(host=redis_host, port=redis_port, db=0)
+        self.ttl = cache_ttl
 
     def add_record(self, hostname, ip_address):
         self.redis.set(hostname, ip_address, ex=self.ttl)
